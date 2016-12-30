70042386a5f4db99c95d1ec982c2b2ce95f2c150
Philippe Schild <philippe.schild@googlemail.com>
2016-12-30 12:21:53 +0100

    (m) bullshifier/ClsJgrvhoeak.java: 99 LOC -> 343 LOC (m) bullshifier/ClsQmebdvv.java: 234 LOC -> 166 LOV

--- bullshifier/src/main/java/ClsJgrvhoeak.java
+++ bullshifier/src/main/java/ClsJgrvhoeak.java
@@ -96,4 +96,248 @@ public class ClsJgrvhoeak {
         int methodToCall = Config.get().getRandom().nextInt(5);
     }
 
+    public static void metIybfa(Context context) throws Exception {
+        int methodId = 1;
+        Map<Object, Object> root = new HashMap();
+        Map<Object, Object> mapValEsuevnpxvhp = new HashMap();
+        Object[] mapValXuctrxoynoj = new Object[9];
+        long valXrqzmydmmxo = -4724256854185976615L;
+
+        mapValXuctrxoynoj[0] = valXrqzmydmmxo;
+        for (int i = 1; i < 9; i++) {
+            mapValXuctrxoynoj[i] = Config.get().getRandom().nextInt(1000);
+        }
+
+
+        Set<Object> mapKeyTzkjvjmcqtk = new HashSet<Object>();
+        String valEspftezyqpc = "StrSfjjzxhobst";
+
+        mapKeyTzkjvjmcqtk.add(valEspftezyqpc);
+        long valTzrtsaouytx = 6526024847393986916L;
+
+        mapKeyTzkjvjmcqtk.add(valTzrtsaouytx);
+
+        mapValEsuevnpxvhp.put("mapValXuctrxoynoj", "mapKeyTzkjvjmcqtk");
+        Set<Object> mapValApuqlyaciqm = new HashSet<Object>();
+        boolean valPgrvkgofwpp = false;
+
+        mapValApuqlyaciqm.add(valPgrvkgofwpp);
+        long valMullryqkmkt = -7375563534535343012L;
+
+        mapValApuqlyaciqm.add(valMullryqkmkt);
+
+        List<Object> mapKeyBofgfhoslno = new LinkedList<Object>();
+        int valTcclrzkqzfk = 38;
+
+        mapKeyBofgfhoslno.add(valTcclrzkqzfk);
+        String valXgpzzzfarki = "StrXcpuwwddrgs";
+
+        mapKeyBofgfhoslno.add(valXgpzzzfarki);
+
+        mapValEsuevnpxvhp.put("mapValApuqlyaciqm", "mapKeyBofgfhoslno");
+
+        Map<Object, Object> mapKeyRspxhxgpgbs = new HashMap();
+        List<Object> mapValCosygumalzw = new LinkedList<Object>();
+        long valIzboccngkoi = -7546356826730405539L;
+
+        mapValCosygumalzw.add(valIzboccngkoi);
+        int valGbiprvbxgyi = 895;
+
+        mapValCosygumalzw.add(valGbiprvbxgyi);
+
+        Object[] mapKeyZaknrlaytmc = new Object[3];
+        int valOigwcvmjywf = 253;
+
+        mapKeyZaknrlaytmc[0] = valOigwcvmjywf;
+        for (int i = 1; i < 3; i++) {
+            mapKeyZaknrlaytmc[i] = Config.get().getRandom().nextInt(1000);
+        }
+
+
+        mapKeyRspxhxgpgbs.put("mapValCosygumalzw", "mapKeyZaknrlaytmc");
+
+        root.put("mapValEsuevnpxvhp", "mapKeyRspxhxgpgbs");
+
+        if (Config.get().shouldThrowSomething(methodId, classId)) {
+            if (Config.get().shouldThrow1000()) {
+                for (int i = 0; i < 1000; i++) {
+                    try {
+                        throw new IllegalStateException("Time for Illegal state exception, context is " + context);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+            if (Config.get().shouldThrowIllegal(context)) {
+                throw new IllegalStateException("Time for Illegal state exception, context is " + context);
+
+            }
+
+            if (Config.get().shouldThrowIO(context)) {
+                throw new IOException("Time for IO exception, context is " + context);
+
+            }
+        }
+
+        if (Config.get().shouldSuicide()) {
+            System.out.println("shouldSuicide.. ");
+            System.exit(0);
+        }
+
+        if (Config.get().shouldRunAway(context)) {
+            return;
+        }
+
+        Config.get().updateContext(context);
+        int methodToCall = Config.get().getRandom().nextInt(5);
+        {
+            int loopIndex25325 = 0;
+            for (loopIndex25325 = 0; loopIndex25325 < 5290; loopIndex25325++) {
+                try {
+                    Integer.parseInt("numBtzrkggbznc");
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            long varLokqlnldryn = (Config.get().getRandom().nextInt(146) + 2);
+            int loopIndex25327 = 0;
+            for (loopIndex25327 = 0; loopIndex25327 < 7846; loopIndex25327++) {
+                java.io.File file = new java.io.File("/");
+
+                if (file.canRead()) {
+                    System.out.println("File exists");
+                } else {
+                    System.out.println("File not exists");
+                }
+            }
+
+        }
+    }
+
+
+    public static void metZbemrxj(Context context) throws Exception {
+        int methodId = 2;
+        Set<Object> root = new HashSet<Object>();
+        Object[] valEmdjfeffrcp = new Object[10];
+        Object[] valUejjrofqrqo = new Object[2];
+        boolean valRgwoeqlqrcz = false;
+
+        valUejjrofqrqo[0] = valRgwoeqlqrcz;
+        for (int i = 1; i < 2; i++) {
+            valUejjrofqrqo[i] = Config.get().getRandom().nextInt(1000);
+        }
+
+
+        valEmdjfeffrcp[0] = valUejjrofqrqo;
+        for (int i = 1; i < 10; i++) {
+            valEmdjfeffrcp[i] = Config.get().getRandom().nextInt(1000);
+        }
+
+
+        root.add(valEmdjfeffrcp);
+
+        if (Config.get().shouldThrowSomething(methodId, classId)) {
+            if (Config.get().shouldThrow1000()) {
+                for (int i = 0; i < 1000; i++) {
+                    try {
+                        throw new IllegalStateException("Time for Illegal state exception, context is " + context);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+            if (Config.get().shouldWriteLogWarn(context)) {
+            }
+
+            if (Config.get().shouldWriteLogError(context)) {
+            }
+
+            if (Config.get().shouldThrowIllegal(context)) {
+                throw new IllegalStateException("Time for Illegal state exception, context is " + context);
+
+            }
+
+            if (Config.get().shouldThrowIO(context)) {
+                throw new IOException("Time for IO exception, context is " + context);
+
+            }
+        }
+
+        if (Config.get().shouldSuicide()) {
+            System.out.println("shouldSuicide.. ");
+            System.exit(0);
+        }
+
+        if (Config.get().shouldRunAway(context)) {
+            return;
+        }
+
+        Config.get().updateContext(context);
+        int methodToCall = Config.get().getRandom().nextInt(5);
+
+        {
+            try {
+                try {
+                    Integer.parseInt("numJdhtreoagtq");
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception ex25336) {
+            } finally {
+                try {
+                    Integer.parseInt("numFiptjpskcla");
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (((3758) % 460322) == 0) {
+                java.io.File file = new java.io.File("/dirLikpemdngsk/dirRgrxjlqbrpv/dirQejjyvulpkl/dirNbwsicaugfq/dirErcgpbrgavw/dirHaazyghmjeu");
+
+                if (file.canRead()) {
+                    System.out.println("File exists");
+                } else {
+                    System.out.println("File not exists");
+                }
+            } else {
+                java.io.File file = new java.io.File("/dirFdhphjnllti");
+
+                if (file.canRead()) {
+                    System.out.println("File exists");
+                } else {
+                    System.out.println("File not exists");
+                }
+            }
+
+            int loopIndex25333 = 0;
+            for (loopIndex25333 = 0; loopIndex25333 < 345; loopIndex25333++) {
+                Object locker = new Object();
+
+                synchronized (locker) {
+                    System.out.println("synchronized block");
+                }
+            }
+
+            try {
+                try {
+                    Integer.parseInt("numGuvwjeryuhb");
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            } finally {
+                try {
+                    Integer.parseInt("numWmznuysjgzr");
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
 }
