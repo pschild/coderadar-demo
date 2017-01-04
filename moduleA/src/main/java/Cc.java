7e85c2eb091dd8bb581cebdd0a0ec6c0416972d0
Philippe Schild <pschild@users.noreply.github.com>
2017-01-04 22:55:47 +0100

    Update Cc.java

--- moduleA/src/main/java/Cc.java
+++ moduleA/src/main/java/Cc.java
@@ -38,4 +38,61 @@ public class Cc {
             }
         }
     }
+    
+    public boolean validate(final BindingResult bindingResult) {
+        boolean validate = true;
+        for (String channel : getConfiguredChannels()) {
+            switch (channel) {
+            case "SMS":
+                // do nothing
+                break;
+            case "Email":
+                // do nothing
+                break;
+            case "Facebook":
+                // do nothing
+break;
+            case "Voice":
+                final SpelExpressionParser parser = new SpelExpressionParser();
+                if (parser
+                        .parseExpression(
+                                "!voiceMessageForm.audioForms.?[audioId == '' || audioId == null].isEmpty()")
+                        .getValue(this, Boolean.class)) {
+                    bindingResult.rejectValue("voiceMessageForm.audioForms",
+                            "message.voice.provide.all.audios");
+                    validate = false;
+                }
+                boolean voiceContentErrorSet = false;
+                    boolean voiceDescriptionErrorSet = false;
+                    for (AudioForm audioForm : (List<AudioForm>) parser
+                            .parseExpression(
+                                    "voiceMessageForm.audioForms.?[description.length() > 8000]")
+                            .getValue(this)) {
+                        if (audioForm.getAddAudioBy().equals(
+                                AudioForm.AddBy.TTS)
+                                && !voiceContentErrorSet) {
+                            voiceContentErrorSet = true;
+                            bindingResult.rejectValue(
+                                    "voiceMessageForm.audioForms",
+                                    "message.voice.content.exceed.limit");
+                        } else {
+                            if (!voiceDescriptionErrorSet) {
+                                voiceDescriptionErrorSet = false;
+                                bindingResult
+                                        .rejectValue(
+                                                "voiceMessageForm.audioForms",
+                                                "message.describe.voice.content.exceed.limit");
+                            }
+                        }
+                        validate = false;
+                    }
+                break;
+            default:
+                throw new IllegalStateException("Unsupported channel: "
+                        + channel);
+            }
+        }
+        return validate;
+    }
+}
 }
