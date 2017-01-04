b2a3a1dd3ff42196146d83a7069e0b3e0b47b2f8
Philippe Schild <pschild@users.noreply.github.com>
2017-01-04 22:47:00 +0100

    Update ClsJgrvhoeak.java

--- bullshifier/src/main/java/ClsJgrvhoeak.java
+++ bullshifier/src/main/java/ClsJgrvhoeak.java
@@ -7,6 +7,46 @@ import java.io.IOException;
 
 public class ClsJgrvhoeak {
     public static final int classId = 166;
+    
+    public void testCyclomaticComplexity()  {               // 1
+       if (a == b)  {                    // 2
+            if (a1 == b1                 // 3
+                && c1 == d1) {   // 4
+               fiddle();
+            }
+            else if (a2 == b2            // 5
+                      || c1 < d1) {   // 6
+                fiddle();
+            }
+            else {
+                fiddle();
+            }
+       }
+        else if (c == d) {               // 7
+            while (c == d) {             // 8
+                fiddle();
+            }
+        }
+         else if (e == f) {
+            for (n = 0; n < h         // 9
+                    || n < 6; n++) {  // 10
+                fiddle();
+            }
+        }
+        else {
+            switch (z) {
+              case 1:                    // 11
+                    fiddle();
+                    break;
+              case 2:                    // 12
+                    fiddle();
+                    break;
+              default:
+                    fiddle();
+                    break;
+            }
+        }
+    }
 
     public static void metZlypv(Context context) throws Exception {
         int methodId = 0;
