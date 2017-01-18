9cf0f0ad9b32cf214473275146ee155235adde4d
Philippe Schild <pschild@users.noreply.github.com>
2017-01-18 21:18:57 +0100

    Update Car.java

--- moduleA/src/main/java/Car.java
+++ moduleA/src/main/java/Car.java
@@ -1,11 +1,18 @@
 public class Car {
   private int wheelsCount = 4;
+	private int gearsCount = 6;
 	private int topSpeed;
 	private int currentSpeed = 0;
 		
 	public void accelerate() {
 		if (currentSpeed < topSpeed) {
 			currentSpeed++;
+		}
+	}
+	
+	public void brake() {
+		if (currentSpeed > 0) {
+			currentSpeed--;
 		}
 	}
 }
