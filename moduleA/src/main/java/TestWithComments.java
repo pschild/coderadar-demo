1f6bb72f6c9e08c3519b0b63dc6540840b81777c
Philippe Schild <pschild@users.noreply.github.com>
2017-01-18 19:46:21 +0100

    Create TestWithComments.java

---
+++ moduleA/src/main/java/TestWithComments.java
@@ -0,0 +1,12 @@
+/**
+ * some class documentation
+ */
+class Test {
+  /**
+   * some method documentation
+   */
+  public Test() {
+    // some line documentation
+    System.out.println("Hello World!");
+  }
+}
