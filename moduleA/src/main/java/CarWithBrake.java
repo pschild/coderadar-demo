7bf4fff5ab7aaa3e6ddea3f7a74208f5b834585f
Philippe Schild <pschild@users.noreply.github.com>
2017-02-04 11:01:12 +0100

    Create CarWithBrake.java

---
+++ moduleA/src/main/java/CarWithBrake.java
@@ -0,0 +1,17 @@
+public class Car {
+  private int wheelsCount = 4;
+	private int topSpeed;
+	private int currentSpeed = 0;
+		
+	public void accelerate() {
+		if (currentSpeed < topSpeed) {
+			currentSpeed++;
+		}
+	}
+	
+	public void brake() {
+		if (currentSpeed > 0) {
+			currentSpeed--;
+		}
+	}
+}
